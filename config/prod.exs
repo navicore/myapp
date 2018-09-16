e527c9d7bee7996c060dd7eebcf146b7f08bf0a7
Ed Sweeney <ed@onextent.com>
2018-09-15 17:23:35 -0700

    init

---
+++ config/prod.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+config :myapp, Myapp.Endpoint,
+  http: [port: "${PORT}"],
+  url: [host: "${HOST}", port: "${PORT}"],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: "${SECRET_KEY_BASE}",
+  server: true,
+  root: "."
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+# Configure your database
+config :myapp, Myapp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  hostname: "${DB_HOSTNAME}",
+  username: "${DB_USERNAME}",
+  password: "${DB_PASSWORD}",
+  database: "${DB_NAME}",
+  pool_size: 20
+
